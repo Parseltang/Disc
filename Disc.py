58ceb13e75440d8174be10a7729b82729165603e
Parseltang <eshenkove@gmail.com>
2022-10-11 21:20:13 +0500

    Add Disc.py

---
+++ Disc.py
@@ -0,0 +1,16 @@
+import math
+
+a = float(input("a="))
+b = float(input("b="))
+c = float(input("c="))
+#(bxb = b*b )
+#ac4= 4*(a*c)
+print()
+D = b * b - 4 * (a * c)
+Ds = math.sqrt(D)
+print("D=" + str(D)+",Корень D=" + str(Ds))
+
+x1 = (-b + Ds) / (2 * a)
+x2 = (-b - Ds) / (2 * a)
+print("x1=" + str(x1), "x2=" + str(x2))
+input()
